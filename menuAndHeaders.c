4a0604246a0f37f98c9af3a12a3a95c2cd6d4edb
mirndlazaro <mirandaalazaro@gmail.com>
2021-11-22 13:30:51 -0300

    :sparkles: Cabeçalhos e menus da aplicação

---
+++ menuAndHeaders.c
@@ -0,0 +1,46 @@
+// O arquivo carrega todos os menus e headers utilizados pelo programa
+
+void menuFuncionalidade()
+{
+	setlocale(LC_ALL, "Portuguese");
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+    printf("      [1] - Incluir novo livro \n      [2] - Consultar livro \n      [3] - Alterar dados de um livro \n      [4] - Ver todos os livros da biblioteca \n      [5] - Sair \n\n");
+	printf("      Digite sua opção: ");
+}
+
+// Cabeçalhos
+void cabecalhoCaso1()
+{
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+	printf("      >>> Incluir novo livro \n");
+}
+
+void cabecalhoCaso2()
+{
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+	printf("      >>> Consultar livro \n");
+}
+
+void cabecalhoCaso3()
+{
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+	printf("      >>> Alterar dados de um livro\n");
+}
+
+void cabecalhoCaso4()
+{
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+	printf("      >>> Ver todos os livros da biblioteca\n");
+}
+
+void cabecalhoCaso5()
+{
+	system("cls");
+	printf("\n      ---- BIBLIOTECA ÌGBOYÀ ----\n\n");
+	printf("      >>> Sair\n");
+}
