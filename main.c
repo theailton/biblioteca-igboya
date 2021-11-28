f0b8393de569e94d2640a917c0a168dae4e3e442
mirndlazaro <mirandaalazaro@gmail.com>
2021-11-28 16:59:03 -0300

    :construction: Incluir funções -m Função incluir livro e ver todos os livros

--- main.c
+++ main.c
@@ -4,40 +4,29 @@
 #include <string.h>
 #include <ctype.h>
 #include <conio.h>
-#include "menuAndHeaders.h"
-#include "funcionalidades.h"
+
+#include "libraries/livro.h"
+#include "libraries/menuAndHeaders.h"
+
+#include "funcoes/funcionalidades.c"
 
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-	inicioLanguage :
+	inicio :
 	menuFuncionalidade();
 	char decisaoFuncionalidade;
 	scanf ("%c", &decisaoFuncionalidade);
 
 	switch (decisaoFuncionalidade)
 	{
-		case '1':
-			incluirLivro();
-			break;
-
-		case '2':
-			consultarLivro();
-			break;
-
-		case '3':
-			editarLivro();
-			break;
-
-		case '4':
-			verTodosLivros();
-			break;
-
-		case '5':
-			exit(0);
-
-		default:
-			goto inicioLanguage;
+		case '1': incluirLivro(); break;
+		case '2': consultarLivro(); break;
+		case '3': editarLivro(); break;
+		case '4': verTodosLivros(); break;
+		case '5': exit(0);
+		default: goto inicio;
 	}
-	goto inicioLanguage;
+	
+	goto inicio;
 }
